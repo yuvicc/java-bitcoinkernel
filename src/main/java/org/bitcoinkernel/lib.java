8f632eb2faf9d44379997b4d30e80b1317e16e2c
yuvicc <yuvichh01@gmail.com>
2025-07-03 12:45:51 +0530

    add: basic java class

---
+++ src/main/java/org/bitcoinkernel/lib.java
@@ -0,0 +1,7 @@
+package org.bitcoinkernel;
+
+public class lib {
+    public static void main(String[] args){
+        System.out.println("Hi there, let's build java wrapper for bitcoin kernel!");
+    }
+}
